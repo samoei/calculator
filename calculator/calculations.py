0702ffc901252bc54c6396d774e3676c70e2c045
Phil Samoei <phil@samphiltech.com>
2022-09-24 16:19:28 +0200

    Further refinement of docs and ignored pycharm configs

--- calculator/calculations.py
+++ calculator/calculations.py
@@ -17,9 +17,13 @@ Examples:
     2.0
 
 The module comtains the following functions:
+
 - `add(a, b)` -  Returns the sum of two numbers.
+
 - `subtract(a, b)` -  Returns the difference of two numbers.
+
 - `multiply(a, b)` -  Returns the product of two numbers.
+
 - `divide(a, b)` -  Returns the quotient of two numbers.
 """
 
